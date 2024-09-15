855c88ce549dbb6cce65e3f9dc99c3435a94b5de
Beyah96 <bahsaid96@gmail.com>
2024-09-16 01:52:17 +0200

    Added solution for Problem 19 : Calculate the area of a circle using Diameter

--- Problem19/Problem19.cpp
+++ Problem19/Problem19.cpp
@@ -1,20 +1,26 @@
-// Problem19.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
-//
-
 #include <iostream>
+#include <string>
+#include <cmath>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+float ReadRadius() {
+	float Diameter;
+	cout << "Please enter the radius : ";
+	cin >> Diameter;
+	return Diameter;
 }
 
-// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
-// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
+float CircleAreaByDiameter(float Diameter) {
+	const float PI = 3.14;
+	return (PI * pow(Diameter, 2)) / 4;
+}
 
-// Astuces pour bien démarrer : 
-//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
-//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
-//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
-//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
-//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
-//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
+void PrintResult(float Area) {
+	cout << "The area of your circle is : " << Area << endl;
+}
+
+int main() {
+
+	PrintResult(CircleAreaByDiameter(ReadRadius()));
+	return 0;
+}
