20f60ae1d9e57e1824f38bc144b8fa1d3d4a3291
Beyah96 <bahsaid96@gmail.com>
2024-09-15 15:27:50 +0200

    Added solution for Problem 4: Hire a driver : CASE 1

---
+++ Problem4/Problem4.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+// Hire a driver case 1
+
+struct stInfo {
+	short Age;
+	bool HasDriverLicense;
+};
+
+stInfo ReadInfo() {
+	stInfo Info;
+
+	cout << "Please enter your age : ";
+	cin >> Info.Age;
+
+	cout << "Do you have a driver license(Yes : 1 / No : 0) : ";
+	cin >> Info.HasDriverLicense;
+
+	return Info;
+}
+
+bool isAccepted(stInfo Info) {
+	return (Info.Age >= 21 && Info.HasDriverLicense);
+}
+
+void PrintResult(bool Decision) {
+	if (Decision) {
+		cout << "Hired" << endl;
+	}
+	else {
+		cout << "Rejected" << endl;
+	}
+}
+
+int main() {
+	PrintResult(isAccepted(ReadInfo()));
+	return 0;
+}
